5764f20901d7a5d7079a4e78d1f70f0d39781ce8
Aaron Bryant <abryantdev@gmail.com>
2023-07-01 12:01:18 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,6 +12,8 @@ import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../assets/logo.png";
 import { Link } from "react-scroll";
 
+import CV from "../assets/SoftwareEngineerCV.pdf";
+
 const Navbar = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
@@ -125,7 +127,8 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="../assets/SoftwareEngineerCV.pdf"
+              download
+              href={CV}
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
