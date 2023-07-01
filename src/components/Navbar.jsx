3d9b837ed9612e2c1ddaa308ceed00565ba39f78
Aaron Bryant <abryantdev@gmail.com>
2023-07-01 11:37:17 -0400

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -125,7 +125,6 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              download
               href="../assets/SoftwareEngineerCV.pdf"
             >
               Resume <BsFillPersonLinesFill size={30} />
