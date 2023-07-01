77b9149431dc56eb23455bb159794d1615a69d54
Aaron Bryant <abryantdev@gmail.com>
2023-07-01 11:45:02 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,14 +19,12 @@ const Home = () => {
           building responsive full-stack web applications.
         </p>
         <div>
-          <Link to="work" smooth={true} duration={500}>
             <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#ea3c12] hover:border-[#ea3c12]'>        
                  View Work            
               <span className='group-hover:rotate-90 duration-300'>
                 <HiArrowNarrowRight className='ml-3 ' />
               </span>
             </button>
-          </Link>
         </div>
       </div>
     </div>
