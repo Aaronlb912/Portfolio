c1eecdc5e9831ad685dc5f79b4e0c7ca37ddd12e
Aaronlb912 <abryant912@hotmail.com>
2025-10-03 18:57:49 -0400

    Initial commit of front-end portfolio

---
+++ src/data/data.js
@@ -0,0 +1,26 @@
+import freddie_mac from '../assets/FreddieMacLogo.svg'
+import jasaron from '../assets/jasaron_learning.png'
+import ibm from '../assets/IBM_logo.svg'
+
+export const data=[
+    {
+        id:1,
+        name:"Freddie Mac",
+        image:freddie_mac,
+        description: "● Commanded the full content pipeline, strategically deploying dynamic assets and ensuring seamless integration within the Drupal architecture to drive a cohesive and compelling user narrative\n● Fortified the stability and integrity of Freddie Mac's mission-critical web ecosystem, executing flawless content updates and proactive maintenance to ensure 100% uptime and operational excellence\n● Spearheaded the technical execution of a complex platform migration, architecting a new front-end experience from legacy systems while rigorously adhering to Freddie Mac design principles"
+    },
+    {
+        id: 2,
+        name: "Jasaron Learning Website",
+        image: jasaron,
+        description: "● Built customized website layout/user interfaces by using standard HTML/CSS practices\n● Maintained websites required by the client and modified websites from layout to function\n● Researched current market trends to increase business"
+    },
+    {
+        id:3,
+        name:"IBM",
+        image:ibm,
+        description:"● Enhanced Drupal's Search query capabilities by including Component searches and reviewed Drupal\nSearch modules to add a Component parameter, facilitating more nuanced searches\n● Addressed customer-reported search issues by developing a script that compared published Drupal\ncontent against the Enterprise Search index, identifying and resolving discrepancies by either removing\nbroken links or repushing unindexed content\n● Managed Drupal sitemaps and automated Google Search updates using Google Search Console; also\nbecame adept at debugging Drupal sitemap issues"
+    },
+
+
+]
