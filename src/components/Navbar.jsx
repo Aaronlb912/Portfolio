0cdd985fcac88df2bc4b932dcc8c7b2d6ae57afe
Aaron Bryant <abryantdev@gmail.com>
2025-10-07 11:55:17 -0400

    Add files via upload
    updated resume to open to new page instead of automatically download

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,5 @@
 import React, { useState } from "react";
-import {
-  FaBars,
-  FaTimes,
-  FaGithub,
-  FaLinkedin,
-} from "react-icons/fa";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../assets/logo.png";
@@ -125,8 +120,8 @@ const Navbar = () => {
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              download
               href={CV}
+              target="_blank"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
